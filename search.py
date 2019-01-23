d26a41abe1871f2046333d18f9379e9b2174b85f
Abhiram Reddy  Duggempudi <40287643+AbhiramReddyDuggempudi@users.noreply.github.com>
2019-01-23 19:13:44 +0530

    Update search.py

--- search.py
+++ search.py
@@ -11,9 +11,23 @@ driver = webdriver.Chrome()
 # I am using chrome for this
 driver.get('https://www.linkedin.com/uas/login')
 #opens the site in a new browser window
-#this time mentioned is for the user to see and login
-time.sleep(25)
-#Woring on searching
-text_area = driver.find_element_by_xpath('//*[@id="ember41"]/input')
-text_area.send_keys("Amitab Bachan")
-driver.quit()
+#this time mentioned is for the user to see
+text_area = driver.find_element_by_id('username')
+#finding where to fill the text
+#sendig the text to be filled
+text_area.send_keys("username")
+text_area = driver.find_element_by_id('password')
+text_area.send_keys("password")
+# click submit button
+submit_button = driver.find_elements_by_xpath('//*[@id="app__container"]/main/div/form/div[3]/button')[0]
+#find search box
+
+
+
+
+
+
+
+#finally closes it
+
+#this is most siple way to start and test the use of selenium
