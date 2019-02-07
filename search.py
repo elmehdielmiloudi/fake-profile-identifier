90c031eaedb9146086bcfa23973fe59c303ebdd9
Abhiram Reddy  Duggempudi <40287643+AbhiramReddyDuggempudi@users.noreply.github.com>
2019-02-07 17:15:49 +0530

    Update search.py

--- search.py
+++ search.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 #pip install selenium
 import time
-import sqlite3 as sq
 #download chrome web driver and add it to path
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -15,35 +14,32 @@ driver.get('https://www.linkedin.com/uas/login')
 text_area = driver.find_element_by_id('username')
 #finding where to fill the text
 #sendig the text to be filled
-text_area.send_keys("username")
+text_area.send_keys("theunmountedprogrammer@gmail.com")
+
 text_area = driver.find_element_by_id('password')
-text_area.send_keys("password")
+
+text_area.send_keys("theunmounted")
 # click submit button
 submit_button = driver.find_elements_by_xpath('//*[@id="app__container"]/main/div/form/div[3]/button')[0]
-#login button
 submit_button.click()
-#clicks it
-#find search box
-search=driver.find_element_by_xpath("//*[@id=\"ember36\"]/input")
+m="amitab/-bachan"
+search=driver.find_element_by_xpath("//*[@id=\"ember35\"]/input")
 search.send_keys("Amitab Bachan","\n")
-#enter the entity name to search
-#Now it loads popular profile results all we need is to collect the profile links
-str=[]
-#collects profile links
+time.sleep(5)
+
+print("Seraching",m)
+res=[]
 for a in driver.find_elements_by_xpath('.//a'):
-    if a.get_attribute("data-control-name")==("search_srp_result"):
-        if a.get_attribute("class")==("search-result__result-link search-result__result-link--visited ember-view"):
-            str.append(a.get_attribute("href"))
+    res.append(a.get_attribute("href"))
+m="amitab-bachan"
 #these if statements are for getting exact links to the resultant profiles
-print("Links are:")
-x=len(str)
-m=0
-for m in range(len(str)):
-    print(str[m])
-
-
-time.sleep(10)
-driver.quit()
+for i in range (len(res)):
+    if m in res[i]:
+        print(res[i])
 
 
 
+
+#finally closes it
+
+#this is most siple way to start and test the use of selenium
